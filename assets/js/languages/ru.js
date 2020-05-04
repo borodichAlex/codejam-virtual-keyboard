b9b993f1e0da7a97398ff8498018fbf6cdc19330
GrootStudent <borodichalexander97@gmail.com>
2020-05-04 21:23:33 +0300

    feat: add layout for languages

---
+++ assets/js/languages/ru.js
@@ -0,0 +1,322 @@
+export default [
+  {
+    lower: 'ё',
+    upper: 'Ё',
+    code: 'Backquote',
+  },
+  {
+    lower: '1',
+    upper: '!',
+    code: 'Digit1',
+  },
+  {
+    lower: '2',
+    upper: '"',
+    code: 'Digit2',
+  },
+  {
+    lower: '3',
+    upper: '№',
+    code: 'Digit3',
+  },
+  {
+    lower: '4',
+    upper: ';',
+    code: 'Digit4',
+  },
+  {
+    lower: '5',
+    upper: '%',
+    code: 'Digit5',
+  },
+  {
+    lower: '6',
+    upper: ':',
+    code: 'Digit6',
+  },
+  {
+    lower: '7',
+    upper: '?',
+    code: 'Digit7',
+  },
+  {
+    lower: '8',
+    upper: '*',
+    code: 'Digit8',
+  },
+  {
+    lower: '9',
+    upper: '(',
+    code: 'Digit9',
+  },
+  {
+    lower: '0',
+    upper: ')',
+    code: 'Digit0',
+  },
+  {
+    lower: '-',
+    upper: '_',
+    code: 'Minus',
+  },
+  {
+    lower: '=',
+    upper: '+',
+    code: 'Equal',
+  },
+  {
+    lower: 'Backspace',
+    upper: null,
+    code: 'Backspace',
+  },
+  {
+    lower: 'Tab',
+    upper: null,
+    code: 'Tab',
+  },
+  {
+    lower: 'й',
+    upper: 'Й',
+    code: 'KeyQ',
+  },
+  {
+    lower: 'ц',
+    upper: 'Ц',
+    code: 'KeyW',
+  },
+  {
+    lower: 'у',
+    upper: 'У',
+    code: 'KeyE',
+  },
+  {
+    lower: 'к',
+    upper: 'К',
+    code: 'KeyR',
+  },
+  {
+    lower: 'е',
+    upper: 'Е',
+    code: 'KeyT',
+  },
+  {
+    lower: 'н',
+    upper: 'Н',
+    code: 'KeyY',
+  },
+  {
+    lower: 'г',
+    upper: 'Г',
+    code: 'KeyU',
+  },
+  {
+    lower: 'ш',
+    upper: 'Ш',
+    code: 'KeyI',
+  },
+  {
+    lower: 'щ',
+    upper: 'Щ',
+    code: 'KeyO',
+  },
+  {
+    lower: 'з',
+    upper: 'З',
+    code: 'KeyP',
+  },
+  {
+    lower: 'х',
+    upper: 'Х',
+    code: 'BracketLeft',
+  },
+  {
+    lower: 'ъ',
+    upper: 'Ъ',
+    code: 'BracketRight',
+  },
+  {
+    lower: '\\',
+    upper: '/',
+    code: 'Backslash',
+  },
+  {
+    lower: 'Del',
+    upper: null,
+    code: 'Delete',
+  },
+  {
+    lower: 'CapsLock',
+    upper: null,
+    code: 'CapsLock',
+  },
+  {
+    lower: 'ф',
+    upper: 'Ф',
+    code: 'KeyA',
+  },
+  {
+    lower: 'ы',
+    upper: 'Ы',
+    code: 'KeyS',
+  },
+  {
+    lower: 'в',
+    upper: 'В',
+    code: 'KeyD',
+  },
+  {
+    lower: 'а',
+    upper: 'А',
+    code: 'KeyF',
+  },
+  {
+    lower: 'п',
+    upper: 'П',
+    code: 'KeyG',
+  },
+  {
+    lower: 'р',
+    upper: 'Р',
+    code: 'KeyH',
+  },
+  {
+    lower: 'о',
+    upper: 'О',
+    code: 'KeyJ',
+  },
+  {
+    lower: 'л',
+    upper: 'Л',
+    code: 'KeyK',
+  },
+  {
+    lower: 'д',
+    upper: 'Д',
+    code: 'KeyL',
+  },
+  {
+    lower: 'ж',
+    upper: 'Ж',
+    code: 'Semicolon',
+  },
+  {
+    lower: 'э',
+    upper: 'Э',
+    code: 'Quote',
+  },
+  {
+    lower: 'Enter',
+    upper: null,
+    code: 'Enter',
+  },
+  {
+    lower: 'Shift',
+    upper: null,
+    code: 'ShiftLeft',
+  },
+  {
+    lower: 'я',
+    upper: 'Я',
+    code: 'KeyZ',
+  },
+  {
+    lower: 'ч',
+    upper: 'Ч',
+    code: 'KeyX',
+  },
+  {
+    lower: 'с',
+    upper: 'С',
+    code: 'KeyC',
+  },
+  {
+    lower: 'м',
+    upper: 'М',
+    code: 'KeyV',
+  },
+  {
+    lower: 'и',
+    upper: 'И',
+    code: 'KeyB',
+  },
+  {
+    lower: 'т',
+    upper: 'Т',
+    code: 'KeyN',
+  },
+  {
+    lower: 'ь',
+    upper: 'Ь',
+    code: 'KeyM',
+  },
+  {
+    lower: 'б',
+    upper: 'Б',
+    code: 'Comma',
+  },
+  {
+    lower: 'ю',
+    upper: 'Ю',
+    code: 'Period',
+  },
+  {
+    lower: '.',
+    upper: ',',
+    code: 'Slash',
+  },
+  {
+    lower: '🠕',
+    upper: null,
+    code: 'ArrowUp',
+  },
+  {
+    lower: 'Shift',
+    upper: null,
+    code: 'ShiftRight',
+  },
+  {
+    lower: 'Ctrl',
+    upper: null,
+    code: 'ControlLeft',
+  },
+  {
+    lower: 'Win', //OSLeft
+    upper: null,
+    code: 'Win',
+  },
+  {
+    lower: 'Alt',
+    upper: null,
+    code: 'AltLeft',
+  },
+  {
+    lower: ' ',
+    upper: null,
+    code: 'Space',
+  },
+  {
+    lower: 'Alt',
+    upper: null,
+    code: 'AltRight',
+  },
+  {
+    lower: '🠔',
+    upper: null,
+    code: 'ArrowLeft',
+  },
+  {
+    lower: '🠗',
+    upper: null,
+    code: 'ArrowDown',
+  },
+  {
+    lower: '🠖',
+    upper: null,
+    code: 'ArrowRight',
+  },
+  {
+    lower: 'Ctrl',
+    upper: null,
+    code: 'ControlRight',
+  },
+];

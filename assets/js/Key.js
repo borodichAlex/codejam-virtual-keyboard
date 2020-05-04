d53f2d317e306c96f2d205e6c7bed2e2cada8c17
GrootStudent <borodichalexander97@gmail.com>
2020-05-04 21:22:51 +0300

    feat: add class Key

--- assets/js/Key.js
+++ assets/js/Key.js
@@ -1,18 +1,30 @@
 export default class Key {
-	constructor({ lower, upper, code }) {
-		this.code = code;
-		this.lower = lower;
-		this.upper = upper;
-		// this.isFnKey = Boolean(lower.match(/Ctrl|arr|Alt|Shift|Tab|Back|Del|Enter|Caps|Win/));
+  constructor({ lower, upper, code }) {
+    this.code = code;
+    this.lower = lower;
+    this.upper = upper;
 
-		if (upper && upper.match(/[^a-zA-Zа-яА-ЯёЁ0-9]/)) {
-			this.sub = create('div', 'sub', this.upper);
-		} else {
-			this.sub = create('div', 'sub', '');
-		}
-
-		this.letter = create('div', 'letter', lower);
-		this.div = create('div', 'keyboard__key', [this.sub, this.letter], null, ['code', this.code],
-			this.isFnKey ? ['fn', 'true'] : ['fn', 'false']);
-	}
+    this.elem = document.createElement('button');
+    this.elem.setAttribute('type', 'button');
+    switch (code) {
+      case 'Backspace':
+      case 'CapsLock':
+      case 'ShiftLeft':
+      case 'ShiftRight':
+      case 'Enter':
+        this.elem.classList.add('keyboard__key', 'keyboard__key--width_middle');
+        break;
+      case 'Space':
+        this.elem.classList.add('keyboard__key', 'keyboard__key--width_large');
+        break;
+      case 'ControlLeft':
+      case 'ControlRight':
+      case 'Tab':
+        this.elem.classList.add('keyboard__key', 'keyboard__key--width_small');
+        break;
+      default:
+        this.elem.classList.add('keyboard__key');
+    }
+    this.elem.textContent = lower;
+  }
 }
